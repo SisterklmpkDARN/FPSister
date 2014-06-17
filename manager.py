6a86c193bbcbbcbb719028d792b88bab42e79ffa
Danang Prawira N <danang.prawira.n@gmail.com>
2014-06-18 02:47:20 +0700

    ganti nama supaya jelas mana file managernya

--- manager.py
+++ manager.py
@@ -1,54 +1,68 @@
-def compute(start, stop):
-    import pickle
+def compute(data, centroids):
     from scipy.spatial.distance import euclidean
-    # load data centroid dengan pickle
-    fp = open('centroids','r')
-    centroids = pickle.load(fp)
-    fp.close()
+    import numpy as np
+    import socket
 
-    # proses test data yang berada pada rentang [start,stop)
-    fp = open('kddcup-testdata.csv','r')
-    res = []
-    for i, line in enumerate(fp):
-        if i >= start:
-            line = line.strip()
-            values = line.split(',')
-            temp = []
-            for val in values: # konversi dari array of string ke array of float
-                temp.append(float(val))
-            idx = 0
-            minn = 1000000000
-            for cent in centroids: # untuk setiap centroid
-                dist = euclidean(temp, cent) # hitung euclidean distance dengan data ini
-                if dist < minn: # TODO: hasil akhirnya seperti apa.. tanya bapaknya lagi
-                    minn = dist
-                    cluster_no = idx
-                idx += 1
-            temp.append(float(idx)) # asumsinya hasil akhirnya data + nomor kluster sbg variabel terakhir
-            res.append(temp)
-        if i == stop:
-	    break
-    fp.close()
-    return res
+    minn = 1000000000
+    idx = 0
+    for j in range(0,len(centroids)):
+        dist = euclidean(data, centroids[j])
+        if dist < minn:
+            minn = dist
+            idx = j
+            
+    data = np.append(data, idx)
+    return data
 
 if __name__ == '__main__':
     import dispy
-    # nodes = alamat ip yg jalanin dispynode, depends = persyaratan yg harus ada di setiap yg jalanin dispynode, ip_addr = ip address yg dipake buat cluster
-    # filenya harus satu folder sama dispynode.py
-    cluster = dispy.JobCluster(compute, nodes=['192.168.1.4', '127.0.0.1'], depends=['kddcup-testdata.csv','centroids'], ip_addr='192.168.1.2')
-    jobs = []
-    start = 0
-    stop = 51838
-    end = 311028 # banyaknya baris di file kddcup-testdata.csv -1 (311029-1)
-    for n in range(6): # looping 6 job
-        job = cluster.submit(start, stop) # jalankan
+    import pickle
+    import numpy as np
+    print 'DEBUG: Creating Job Cluster'
+    cluster = dispy.JobCluster(compute)
+    jobs = np.array([])
+    print 'DEBUG: Reading data'
+    # load data centroid dengan pickle
+    fp = open('centroids_np','r')
+    centroids = pickle.load(fp)
+    #centroids = np.array(centroids)
+    fp.close()
+    # load file kddcup-testdata
+    fp = open('kddcup-testdata.csv','r')
+    #sz = 311028
+    n = 0
+    print 'DEBUG: Distributing jobs'
+    for i,line in enumerate(fp):
+        line = line.strip()
+        temp = line.split(',')
+        lis = np.array([])
+        for val in temp:
+            lis = np.append(lis, float(val))
+        print 'DEBUG: Submit job',n+1
+        job = cluster.submit(lis, centroids)
         job.id = n
-        jobs.append(job)
-        start = stop
-	stop += stop
-	if stop == end
-            stop = end+1
+        jobs = np.append(jobs, job)
+        n += 1
+
+    np.set_printoptions(precision=3)
+    hasil = np.array([])
+    
     for job in jobs:
         res = job()
-        # TODO: hasil akhirnya seperti apa..
-    cluster.stats()
+        if hasil.size == 0:
+            hasil = np.append(hasil, res)
+        else:
+            hasil = np.vstack((hasil,res))
+        print '%s executed job %s at %s' % (job.ip_addr, job.id, job.start_time)
+        #hasil = np.append(hasil, res)
+
+    print hasil
+    print 'DEBUG: Writing to file'
+    np.savetxt('test.txt',hasil,fmt='%.2f',delimiter=',',newline=' ')
+'''    fp = open('hasil.txt','w')
+    for data in hasil:
+        fp.write(data)
+        fp.write('\n')
+    fp.close()'''
+
+cluster.stats()
